0aaae3d61f24ee0e99e5a940027a0dbf5eefbfef
Moses Gathuku <mosesgathuku95@gmail.com>
2019-07-07 23:45:07 +0300

    wip

--- test/controllers/transaction_controller_test.rb
+++ test/controllers/transaction_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class TransactionControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @transaction=transactions(:one) # from fixtures
+    @user=User.new(name:'John Doe',email:'johndoe@gmail.com',password:'password',
+                   authentication_token:SecureRandom.alphanumeric, wallet:10.0)
+  end
+
+  test 'should have transfer' do
+    post '/transaction/tranfer', params: {amount:@transaction,made_to:@transaction.made_to}
+      assert_response :success
+  end
 end
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+   def setup
+     @user=users(:valid)
+   end
+
+   test 'should get login' do
+     post '/user/login'
+     assert_response :success
+   end
+
+   test 'should get register' do
+     post '/user/register', params: {name:@user.name, email:@user.email, password:@user.password_digest}
+      assert_response :success
+   end
+
+   test 'should get logout' do
+     delete '/user/logout'
+     assert_response :success
+   end
+
 end
