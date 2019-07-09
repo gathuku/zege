d3c1db7603deda1dc01e4c70eb50ac643d59010f
Moses Gathuku <mosesgathuku95@gmail.com>
2019-07-09 10:27:32 +0300

    test fix

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -4,4 +4,19 @@ class SessionControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user=users(:valid)
+  end
+
+  test 'should get login' do
+    post '/user/login'
+    assert_response :success
+  end
+
+  test 'should get logout' do
+    delete '/user/logout'
+    assert_response :success
+  end
+
+
 end
--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -5,19 +5,9 @@ class UserControllerTest < ActionDispatch::IntegrationTest
      @user=users(:valid)
    end
 
-   test 'should get login' do
-     post '/user/login'
-     assert_response :success
-   end
-
    test 'should get register' do
      post '/user/register', params: {name:@user.name, email:@user.email, password:@user.password_digest}
       assert_response :success
-   end
-
-   test 'should get logout' do
-     delete '/user/logout'
-     assert_response :success
    end
 
 end
