05f61413ba2d05367979385fed432dd23d8de042
Moses Gathuku <mosesgathuku95@gmail.com>
2019-07-11 10:24:16 +0300

    push

--- app/controllers/validates/user.rb
+++ app/controllers/validates/user.rb
@@ -4,6 +4,7 @@ module Validate
     attr_accessor :name, :email, :password
 
     def initialize(params={})
+      
       @name=params[:name]
       @email=params[:email]
       @password=params[:password]
